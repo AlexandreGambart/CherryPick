3ca14023c5c2d21492f944d19f0590c132474381
Rakesh Nallapeta Eshwaraiah <rakeshh91@gmail.com>
2016-04-20 18:21:57 -0400

    Added logo for logout button. Showcase view in homepage fragment added.

--- app/src/main/java/com/example/nitishbhaskar/cherrypick/HomePageFragment.java
+++ app/src/main/java/com/example/nitishbhaskar/cherrypick/HomePageFragment.java
@@ -8,12 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence;
+import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomePageFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private static final String SHOWCASE_ID = "Sequence 1";
 
     public static HomePageFragment newInstance(int sectionNumber){
         HomePageFragment homePageFragment = new HomePageFragment();
@@ -41,6 +45,27 @@ public class HomePageFragment extends Fragment {
             throw new ClassCastException("Implementation missed out.");
         }
         Button buyTile = (Button) view.findViewById(R.id.buyTile);
+        Button sellTile = (Button)view.findViewById(R.id.sellTile);
+        Button exchangeTile = (Button)view.findViewById(R.id.exchangeTile);
+
+        ShowcaseConfig config = new ShowcaseConfig();
+        config.setDelay(500); // half second between each showcase view
+
+        MaterialShowcaseSequence sequence = new MaterialShowcaseSequence(getActivity(), SHOWCASE_ID);
+
+        sequence.setConfig(config);
+
+        sequence.addSequenceItem(buyTile,
+                "Click this button to see all the products that are on sale", "GOT IT");
+
+        sequence.addSequenceItem(sellTile,
+                "Click this button to sell your product", "GOT IT");
+
+        sequence.addSequenceItem(exchangeTile,
+                "Click this button to exchange your product with other products", "GOT IT");
+
+        sequence.start();
+
         buyTile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
