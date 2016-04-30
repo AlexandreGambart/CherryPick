bd58b43bb01dc7248b7eba311c6466ac4c7db13b
Rakesh Nallapeta Eshwaraiah <rakeshh91@gmail.com>
2016-04-29 21:10:46 -0400

    Added sliding panel to the about me activity.

--- app/src/main/java/com/example/nitishbhaskar/cherrypick/AboutUsActivity.java
+++ app/src/main/java/com/example/nitishbhaskar/cherrypick/AboutUsActivity.java
@@ -1,13 +1,138 @@
 package com.example.nitishbhaskar.cherrypick;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.ViewGroup;
 
-public class AboutUsActivity extends Activity {
+import com.firebase.client.Firebase;
 
+public class AboutUsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    ViewGroup mAppBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mAppBar = (ViewGroup)findViewById(R.id.appBarAboutus);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        setTitle("About Us");
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        final SlidingPaneLayout layout = (SlidingPaneLayout) findViewById(R.id.sliding_pane_layout);
+        layout.setSliderFadeColor(Color.TRANSPARENT);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Intent intent;
+        if (id == R.id.Homepage) {
+            intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } else if (id == R.id.buyNavigation) {
+            intent = new Intent(this, BuyActivity.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(this,this.mAppBar, "testTransition");
+                startActivity(intent, options.toBundle());
+            }
+            else {
+                startActivity(intent);
+            }
+
+        } else if (id == R.id.sellNavigation) {
+            intent = new Intent(this,SellActivity.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(this,this.mAppBar, "testTransition");
+                startActivity(intent, options.toBundle());
+            }
+            else {
+                startActivity(intent);
+            }
+
+        } else if (id == R.id.exchangeNavigation) {
+            intent = new Intent(this, MyPageActivity.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(this,this.mAppBar, "testTransition");
+                startActivity(intent, options.toBundle());
+            }
+            else {
+                startActivity(intent);
+            }
+        } else if (id == R.id.logoutApp) {
+            Firebase ref = new Firebase(getString(R.string.firebaseUrl));
+            ref.unauth();
+            intent = new Intent(this, LoginActivity.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(this,this.mAppBar, "testTransition");
+                startActivity(intent, options.toBundle());
+            }
+            else {
+                startActivity(intent);
+            }
+        }
+        else if (id == R.id.aboutUs) {
+            intent = new Intent(this, AboutUsActivity.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(this,this.mAppBar, "testTransition");
+                startActivity(intent, options.toBundle());
+            }
+            else {
+                startActivity(intent);
+            }
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        int count = getFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+
+            super.onBackPressed();
+            Intent intent = new Intent(AboutUsActivity.this,MainActivity.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                startActivity(intent);
+            }
+            else {
+                startActivity(intent);
+            }
+
+
+            //additional code
+        } else {
+            getFragmentManager().popBackStack();
+        }
+
     }
 }
